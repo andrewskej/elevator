3e31b213f7309fb53a71d2238a26d11be6f9530e
andrewskej <andrewskej@gmail.com>
2019-03-25 23:16:20 +0900

    something's wrong

--- src/Contexts/ElevatorStore.js
+++ src/Contexts/ElevatorStore.js
@@ -26,7 +26,10 @@ class ElevatorProvider extends Component {
             const levels = [1,2,3,4,5,6,7,8,9]
             const positions = [4,14,24,34,44,54,64,74,84]
             const targetPos = positions[levels.indexOf(Number(calledFrom))]
-            this.actions.findClosest(targetPos, direction)
+            const selectedCar = this.actions.findClosest(targetPos, direction)
+            const howManyFloors = (targetPos - this.state[selectedCar])/10
+
+            this.actions.moveCar(selectedCar,direction,howManyFloors)
         },
 
         findClosest : (targetPos, direction) => {
@@ -40,24 +43,39 @@ class ElevatorProvider extends Component {
             const moveThis = Number(diff.indexOf(smallest))+1
 
             //up or down?, move how many floors?
-            this.actions.moveOneUp('car'+moveThis+'Pos')
+            let selected = `car${moveThis}Pos`
+            return selected
             
         },
 
 
 
         //현재 위치에 10% 더하기
-        moveOneUp:(target) => {
-            this.setState({
-                [target]:this.state[target] + 10
-            })
-        },
+        // moveOneUp:(target) => {
+        //     this.setState({
+        //         [target]:this.state[target] + 10
+        //     })
+        // },
 
-        moveOneDown:(target)=>{
-            this.setState({
-                [target]:this.state[target] - 10
-            })
-        },
+        // moveOneDown:(target)=>{
+        //     this.setState({
+        //         [target]:this.state[target] - 10
+        //     })
+        // },
+
+        //something's fuckin wrong
+        moveCar:(target,direction,howManyFloors) => {
+            console.log(target, howManyFloors, direction)
+
+            let dir = direction ==='u' ? 10 : (-10)
+            for(let i=0; i<howManyFloors;i++){
+                setTimeout(()=>{
+                    this.setState({
+                        [target]:this.state[target] + dir
+                    })
+                },1500)
+            }
+        }
 
     }
 
